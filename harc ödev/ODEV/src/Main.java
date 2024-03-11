f4c07b3032e1ae8b11fc1cc7d98403e73e27ad96
dgknylz <162124054+dgknylz@users.noreply.github.com>
2024-03-11 23:34:23 +0300

    Main.java

--- harc ödev/ODEV/src/Main.java
+++ harc ödev/ODEV/src/Main.java
@@ -1,23 +1,28 @@
 public class Main {
     public static void main(String[] args) throws IllegalAccessException {
         Ogrenci[] ogrenciler = {
-                new Ogrenci("2023141024", 0.0),
-                new Ogrenci("İbadullah", "Bilgisayar Müh", "2023141039", 2.41, 2023),
-                new Ogrenci("Sevim", "Makine Müh", "2017147027",2.17 , 2017),
-                new Ogrenci("Kasım", "Elektrik-Elektronik Müh", "2018104011", 1.98, 2018)
+                new Ogrenci(0.0),
+                new Ogrenci("Gökhan", "Bilgisayar Müh", 2023,0.0 ,102,324),
+                new Ogrenci("Ayşe", "Makine Müh", 2015,0.0 ,103,258),
+                new Ogrenci("Elif", "Elektrik-Elektronik Müh", 2017,1.98,104,152)
         };
 
         System.out.println("Öğrenci Bilgileri");
         for (int i = 0; i < ogrenciler.length; i++) {
             ganoCont(ogrenciler[i].getGano());
-            System.out.println("OGR-" + (i+1) + " " + ogrenciler[i]);
+            System.out.println("OGR-" + (i+1) + " " + ogrenciler[i]+ " ");
+
+
+
         }
         for (int i = 1; i < ogrenciler.length; i++) {
             if (ogrenciler[i].harcHesapla()>0) {
-                System.out.println(i+". Öğrencinin Ödeyeceği harc: "+ ogrenciler[i].harcHesapla());
+                System.out.println(i+1+". Öğrencinin Ödeyeceği harc: "+ ogrenciler[i].harcHesapla());
             }
 
         }
+
+
     }
     public static void ganoCont (double gano) throws IllegalAccessException {
         if (gano<0 || gano>4){
